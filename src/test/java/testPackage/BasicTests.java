68fcb4b42b2d1bd340237da394a18884e8e0f755
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-08-17 14:19:31 +0300

    exploring driver methods

--- src/test/java/testPackage/BasicTests.java
+++ src/test/java/testPackage/BasicTests.java
@@ -1,16 +1,62 @@
 package testPackage;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class BasicTests {
 
     @Test
-    public void basicTests() {
+    public void basicNavigationTest() {
         WebDriver driver;
         driver = new ChromeDriver();
         driver.get("https://www.selenium.dev/selenium/web/web-form.html");
+        driver.quit();
+    }
+
+    //@Test
+    public void browserActionsTest() {
+        WebDriver driver;
+        driver = new ChromeDriver();
+
+        driver.get("");
+        driver.navigate().to("");
+        driver.navigate().back();
+        driver.navigate().forward();
+        driver.navigate().refresh();
+
+        driver.quit();
+        driver.close();
+
+        driver.getCurrentUrl();
+        driver.getPageSource();
+        String windowTitle = driver.getTitle();
+        var windowTitle2 = driver.getTitle();
+
+        //////
+        driver.getWindowHandle();
+        driver.getWindowHandles();
+
+        driver.switchTo().window("");
+        driver.switchTo().newWindow(WindowType.TAB).get("");
+        driver.switchTo().activeElement();
+
+        driver.switchTo().alert().accept();
+        driver.switchTo().alert().dismiss();
+        driver.switchTo().alert().getText();
+        driver.switchTo().alert().sendKeys("");
+
+        driver.switchTo().frame(0);
+        driver.switchTo().frame("");
+        driver.switchTo().parentFrame();
+
+        driver.switchTo().defaultContent();
+        //////
+
+//        driver.findElement();
+//        driver.findElements();
+
         driver.quit();
     }
 }
