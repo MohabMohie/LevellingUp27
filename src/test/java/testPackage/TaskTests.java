34afd15de195f614e73c6466d1c1eb9d86b106a1
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-08-17 15:11:08 +0300

    implementing task#1 (#1)

---
+++ src/test/java/testPackage/TaskTests.java
@@ -0,0 +1,35 @@
+package testPackage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TaskTests {
+
+    /**
+     * ________________ Task #1 - Difficulty: Basic ________________
+     * Open Google Chrome
+     * Navigate to [https://www.google.com/ncr]
+     * Assert that the page title is [Google]
+     * Close Google Chrome
+     */
+    @Test
+    public void task1() {
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.navigate().to("https://www.google.com/ncr");
+        var title = driver.getTitle();
+        System.out.println(title);
+
+        Assert.assertEquals(title, "Google");
+        driver.quit();
+    }
+
+    @Test
+    public void task2() {
+        System.out.println("Google");
+    }
+}
