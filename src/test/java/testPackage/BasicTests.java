21992a788040c9fd2a3d7a22d9c7c5654cc2aacc
Mohab Mohie <Mohab.MohieElDeen@outlook.com>
2024-08-23 11:20:01 +0300

    driver.manage + synchronization

--- src/test/java/testPackage/BasicTests.java
+++ src/test/java/testPackage/BasicTests.java
@@ -1,9 +1,14 @@
 package testPackage;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WindowType;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class BasicTests {
 
@@ -56,6 +61,56 @@ public class BasicTests {
 
 //        driver.findElement();
 //        driver.findElements();
+
+        driver.manage().window().maximize();
+        driver.manage().window().minimize();
+        driver.manage().window().fullscreen();
+        driver.manage().window().getPosition();
+        driver.manage().window().getSize();
+        driver.manage().window().setPosition(new Point(0,0));
+        driver.manage().window().setSize(new Dimension(1920,1080)); //1080p, 720p
+
+        driver.manage().timeouts().getScriptTimeout();
+        driver.manage().timeouts().getImplicitWaitTimeout();
+        driver.manage().timeouts().getPageLoadTimeout();
+
+        driver.manage().timeouts().scriptTimeout(Duration.ofSeconds(10));
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
+
+        //waiting strategies (synchronization)
+//        Thread.sleep(3000);
+
+        //implicit wait
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.findElement(By.xpath("")).click();
+
+        Wait<WebDriver> wait;
+//        explicit wait
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        //old -> expected conditions
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath(""))).click();
+//        driver.findElement(By.xpath("")).isDisplayed();
+//        driver.findElement(By.xpath("")).isEnabled();
+//        driver.findElement(By.xpath("")).click();
+
+        //new -> lambda expression
+        wait.until(d -> {
+            driver.findElement(By.xpath("")).click();
+            return true;
+        });
+
+//        custom wait (fluent wait)
+        wait = new FluentWait<>(driver)
+                        .withTimeout(Duration.ofSeconds(30))
+                        .pollingEvery(Duration.ofMillis(300))
+                        .ignoring(NotFoundException.class)
+                        .ignoring(ElementNotInteractableException.class)
+                        .ignoring(StaleElementReferenceException.class);
+
+        wait.until(d -> {
+            driver.findElement(By.xpath("")).click();
+            return true;
+        });
 
         driver.quit();
     }
